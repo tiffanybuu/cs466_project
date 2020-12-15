2874b17cebadea61e83ce8e2f98971ea521bb80c
Abhinav Pappu <abhi1pappu9@gmail.com>
2020-12-14 21:41:10 -0600

    Add function for connecting to API with type definitions

---
+++ frontend/src/api.ts
@@ -0,0 +1,13 @@
+const API = 'https://nussinov-api.vercel.app';
+
+type NussinovData = {
+  dpTable: number[][],
+  maxScore: number,
+  pairings: [number, number][], // array of tuples containing 2 numbers
+  dashStructure: string,
+}
+
+export function nussinov(rnaStrand: string, minLoopParameter: number): Promise<NussinovData> {
+  return fetch(`${API}/?rna=${rnaStrand}&minloop=${minLoopParameter}`)
+    .then((response) => response.json());
+}
