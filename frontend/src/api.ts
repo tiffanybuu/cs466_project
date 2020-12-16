a32a75371988ef8920ac932f9beb948fc31d7860
Alice Fang <alicesf2@illinois.edu>
2020-12-15 19:34:54 -0600

    replace localhost with production url

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,5 +1,4 @@
-// const API = 'https://nussinov-api.vercel.app';
-const API = 'http://127.0.0.1:5000';
+const API = 'https://nussinov-api.vercel.app';
 
 type NussinovData = {
   dpTable: number[][],
