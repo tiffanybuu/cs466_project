fd1293d8bf9a80b3f457505a5e087d226745d3cf
Alice Fang <alicesf2@illinois.edu>
2020-12-17 02:06:38 -0600

    added hairpin loop slider and repositioned inputs and data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
 import React, {useState, useEffect, useMemo, useCallback} from 'react';
-import { Button, Form, Table } from 'semantic-ui-react'
+import { Button, Dimmer, Form, Grid, Header, Icon, Input, Loader, Segment, Table } from 'semantic-ui-react'
+// @ts-ignore
+import { Slider } from "react-semantic-ui-range";
 import { nussinov, NussinovData } from './api'
 import { traceback, Step } from './traceback';
 import './App.css';
@@ -8,10 +10,26 @@ const ANIMATION_STEP_DURATION = 1250;
 const ANIMATION_TRANSITION_DURATION = 500;
 
 function App() {
+  const [loading, setLoading] = useState(false)
   const [rnaStrand, setRnaStrand] = useState("")
-  const [minLoopParam, setMinLoopParam] = useState(0)
-  const [nussinovData, setNussinovData] = useState<NussinovData>()
   const [rnaCopy, setRnaCopy] = useState("")
+  const [minLoopParam, setMinLoopParam] = useState(0)
+  const sliderSettings = {
+    min: 0,
+    max: 30,
+    step: 1,
+    onChange: (value: number) => {
+      setMinLoopParam(value)
+    }
+  }
+  const calculateNussinov = async () => {
+    const response = await nussinov(rnaStrand, minLoopParam);
+    setSteps(traceback(rnaStrand, response.dpTable));
+    setNussinovData(response)
+    setRnaCopy(rnaStrand)
+    setMinLoopParam(minLoopParam)
+  }
+  const [nussinovData, setNussinovData] = useState<NussinovData>()
   const [steps, setSteps] = useState<Step[]>([]);
   const [currentStep, setCurrentStep] = useState(-1); // -1 will cause entire backtrace to be displayed
 
@@ -22,13 +40,16 @@ function App() {
     setRnaStrand(event.target.value)
   }
   const handleLoopChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setMinLoopParam(parseInt(event.target.value))
+    let value = parseInt(event.target.value)
+    if (!value) {
+      value = 0
+    }
+    setMinLoopParam(value)
   }
-  const handleClick = async () => {
-    const response = await nussinov(rnaStrand, minLoopParam);
-    setSteps(traceback(rnaStrand, response.dpTable));
-    setNussinovData(response)
-    setRnaCopy(rnaStrand)
+  const handleClick = () => {
+    setLoading(true)
+    calculateNussinov()
+    setLoading(false)
   }
 
   const handlePlayAnimation = useCallback(() => {
@@ -132,66 +153,104 @@ function App() {
   return (
     <div className="App">
       <h1 className="header">Interactive Nussinov Visualizer</h1>
-      <Form>
-        <Form.TextArea label='RNA Input Sequence' placeholder='Enter input sequence...' onChange={(event) => handleRnaChange(event)} />
-        <br />
-        <Form.Input label="Minimum Length for Hairpin Loops" placeholder='Enter an integer...' onChange={(event) => handleLoopChange(event)} />
-        <br />
-        <Button type='submit' onClick={handleClick}>Run Nussinov!</Button>
-      </Form>
-      {nussinovData &&
-        <>
-          <h4>Dash Structure: {nussinovData.dashStructure}</h4>
-          <h4>Max # of Pairings: {nussinovData.maxScore}</h4>
-          <div className="dp-table">
-            <Table compact celled definition>
-              <Table.Header>
-                <Table.Row>
-                  <Table.HeaderCell />
-                  {nussinovData && nussinovData.dashStructure.split(" ").map(x => (
-                    <Table.HeaderCell>
-                      {x}
-                    </Table.HeaderCell>
-                  ))}
-                </Table.Row>
-                <Table.Row>
-                  <Table.HeaderCell />
-                  {nussinovData && rnaCopy.split("").map(x =>
-                    <Table.HeaderCell>
-                      {x}
-                    </Table.HeaderCell>
-                  )}
-                </Table.Row>
-              </Table.Header>
-              <Table.Body>
-                {nussinovData && nussinovData.dpTable.map((row, i) =>
+      <Grid>
+        <Grid.Column width={3}>
+          <Form>
+            <p><b>RNA Input Sequence</b></p>
+            <Form.TextArea placeholder='Enter input sequence...' value={rnaStrand} onChange={(event) => handleRnaChange(event)} />
+            <br />
+            <p><b>Minimum Hairpin Loop Length</b></p>
+            <Form.Input 
+              placeholder='Enter an integer or use slider below...'
+              onChange={(event) => handleLoopChange(event)}/>
+            <br />
+            <div className="loop-input">
+                <Slider value={minLoopParam} color="red" settings={sliderSettings} />
+                <p className="slider-left">0</p>
+                <p className="slider-right">30</p>
+            </div>
+            <p>{minLoopParam}</p>
+            <br />
+            <Button onClick={handleClick}>Run Nussinov!</Button>
+          </Form>
+          <br />
+          {nussinovData &&
+            <Segment>
+              <p><b>Dash Structure:</b></p>
+              <p>{nussinovData.dashStructure}</p>
+              <br />
+              <p><b>Max Pairings:</b></p>
+              <p>{nussinovData.maxScore}</p>
+              <br />
+              <p><b>Play Animation</b></p>
+              <Button.Group>
+                <Button icon="chevron left" title="Step Backward (Left Arrow Key)" onClick={handleStepBackward} />
+                <Button icon="stop" title="Stop Animation" onClick={handleStopAnimation} />
+                <Button
+                  icon={isAnimationPlaying ? 'pause' : 'play'}
+                  title={`${isAnimationPlaying ? 'Pause' : 'Play'} Animation (Space)`}
+                  onClick={isAnimationPlaying ? handlePauseAnimation : handlePlayAnimation}
+                />
+                <Button icon="chevron right" title="Step Forward (Right Arrow Key)" onClick={handleStepForward} />
+              </Button.Group>
+            </Segment>
+          }
+        </Grid.Column>
+        <Grid.Column width={13}>
+          {nussinovData ?
+          <>
+            <div className="dp-table">
+              <Table compact celled definition>
+                <Table.Header>
                   <Table.Row>
-                    <Table.Cell>
-                      {rnaCopy[i]}
-                    </Table.Cell>
-                    {row.map((score, j) =>
-                      getTableCell(score, i, j)
+                    <Table.HeaderCell />
+                    {nussinovData.dashStructure.split(" ").map(x => (
+                      <Table.HeaderCell>
+                        {x}
+                      </Table.HeaderCell>
+                    ))}
+                  </Table.Row>
+                  <Table.Row>
+                    <Table.HeaderCell />
+                    {rnaCopy.split("").map(x =>
+                      <Table.HeaderCell>
+                        {x}
+                      </Table.HeaderCell>
                     )}
                   </Table.Row>
-                )}
-              </Table.Body>
-            </Table>
+                </Table.Header>
+                <Table.Body>
+                  {nussinovData.dpTable.map((row, i) =>
+                    <Table.Row>
+                      <Table.Cell>
+                        {rnaCopy[i]}
+                      </Table.Cell>
+                      {row.map((score, j) =>
+                        getTableCell(score, i, j)
+                      )}
+                    </Table.Row>
+                  )}
+                </Table.Body>
+              </Table>
+            </div>
+          </> :
+          <div className="dp-table">
+            <Segment placeholder>
+              {loading ? 
+                <Dimmer active inverted>
+                  <Loader size='massive'>Loading...</Loader> 
+                </Dimmer>
+                :
+                <Header icon>
+                  <Icon name='table' />
+                  Traceback table will be displayed here.
+                </Header>
+              }
+            </Segment>
           </div>
-
-          <br />
-          <Button.Group>
-            <Button icon="chevron left" title="Step Backward (Left Arrow Key)" onClick={handleStepBackward} />
-            <Button icon="stop" title="Stop Animation" onClick={handleStopAnimation} />
-            <Button
-              icon={isAnimationPlaying ? 'pause' : 'play'}
-              title={`${isAnimationPlaying ? 'Pause' : 'Play'} Animation (Space)`}
-              onClick={isAnimationPlaying ? handlePauseAnimation : handlePlayAnimation}
-            />
-            <Button icon="chevron right" title="Step Forward (Right Arrow Key)" onClick={handleStepForward} />
-          </Button.Group>
-          <br /><br />
-        </>
-      }
+        }          
+        </Grid.Column>
+      </Grid>
     </div>
   );
 }
